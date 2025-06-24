0c19d68192b29d4d53cbafe773c057c8e0bb1f86
Ayush Kumar Jha <jhakrayush2007@gmail.com>
2025-06-24 12:28:40 +0530

    Organised The Files

---
+++ frontend/scripts/landing.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const convoButton = document.querySelector(".convo");
+
+  convoButton.addEventListener("click", function () {
+    alert("Chatbot starting... (you can plug in your bot here!)");
+
+  });
+
+  if (window.location.hash) {
+    history.replaceState(null, null, ' ');
+    window.scrollTo(0, 0);
+  }
+});
+
